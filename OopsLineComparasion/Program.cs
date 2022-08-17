b143ee4fa4bea9b6b52496e119385500b79e230a
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-17 14:10:12 +0530

    [Hemant] Added UC2 Equality of Lines

--- OopsLineComparasion/Program.cs
+++ OopsLineComparasion/Program.cs
@@ -10,14 +10,13 @@ namespace OopsLineComparasion
 
             bool check = true;
 
-            int resilt = 0;
-
             //start : is used to repeat again using goto
             start:
             while (check)
             {
                 Console.WriteLine("Select option  :\n" + 
-                    "1)To Calculate Length of Line");
+                    "1)To Calculate Length of Line\n " + 
+                    "2)To Check Equality Of Line\n ");
 
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
@@ -28,6 +27,33 @@ namespace OopsLineComparasion
                         Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
                         Console.WriteLine("Length of the line is :" + result );
                         Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter co-ordinates of Line No.1 : ");
+                        LineComparasion line1= new LineComparasion();
+                        double result1 = line1.LineLength();
+                        Console.WriteLine("length of Line no.1 is :" + result1);
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
+
+
+                        Console.WriteLine("Enter co-ordinates of Line No.2 : ");
+                        LineComparasion line2 = new LineComparasion();
+                        double result2 = line2.LineLength();
+                        Console.WriteLine("length of Line no.2 is :" + result2);
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
+
+
+                        Console.WriteLine("Now checking Equality of Line");
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
+                        if (result1 == result2)
+                        {
+                            Console.WriteLine("Both Line lengths are Equal");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Both Line Lengths are Not Equal");
+                        }
                         break;
 
                     default:
