bfac9edee54650a1988ae60e0e6c62d8bff97b7f
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-17 15:17:42 +0530

    [Hemant] Added UC3 Check whether length is greater or less

--- OopsLineComparasion/Program.cs
+++ OopsLineComparasion/Program.cs
@@ -15,8 +15,9 @@ namespace OopsLineComparasion
             while (check)
             {
                 Console.WriteLine("Select option  :\n" + 
-                    "1)To Calculate Length of Line\n " + 
-                    "2)To Check Equality Of Line\n ");
+                    "1)To Calculate Length of Line\n" + 
+                    "2)To Check Equality Of Line\n" + 
+                    "3)To Check Length of Line is greater or less\n");
 
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
@@ -46,7 +47,7 @@ namespace OopsLineComparasion
 
                         Console.WriteLine("Now checking Equality of Line");
                         Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
-                        if (result1 == result2)
+                        if (result1.Equals(result2))
                         {
                             Console.WriteLine("Both Line lengths are Equal");
                         }
@@ -56,7 +57,39 @@ namespace OopsLineComparasion
                         }
                         break;
 
-                    default:
+                    case 3:
+                        Console.WriteLine("Enter co-ordinates of Line No.1 : ");
+                        LineComparasion line3 = new LineComparasion();
+                        double result3 = line3.LineLength();
+                        Console.WriteLine("length of Line no.1 is :" + result3);
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
+
+
+                        Console.WriteLine("Enter co-ordinates of Line No.2 : ");
+                        LineComparasion line4 = new LineComparasion();
+                        double result4 = line4.LineLength();
+                        Console.WriteLine("length of Line no.2 is :" + result4);
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
+
+
+                        Console.WriteLine("Now Comparing Length of Lines as :");
+
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
+                        if (result3.Equals(result4))
+                        {
+                            Console.WriteLine("Length of Line No. 1 is Equal to Line No. 2");
+                        }
+                        else if (result3.CompareTo(result4) > 0)
+                        {
+                            Console.WriteLine("Length of Line No. 1 is greater than Line No. 2");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Length of Line No. 1 is less than Line No. 2");
+                        }
+                       break ;
+
+                        default:
                         Console.WriteLine("Please enter proper option");
                         break;
 
